270becd047527fba7ea9006998fdd634a00b4a17
hotibody99828 <lyybigber1616@gmail.com>
2026-09-18 18:14:20 +0700

    Update TeleportSystem.lua

--- Features/TeleportSystem.lua
+++ Features/TeleportSystem.lua
@@ -28,7 +28,7 @@ end)
 -- ==================================================
 local SAFE_ZONE = Vector3.new(533, 70, -366)
 
-local FLY_SPEED = 1150
+local FLY_SPEED = 1130
 local RETURN_SPEED = 900
 local FLY_OFFSET = 40
 local SHOT_DISTANCE = 30
