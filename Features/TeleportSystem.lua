675f3e7891ac5b0635b7f1a4c922611f4979718f
hotibody99828 <lyybigber1616@gmail.com>
2026-09-19 19:18:34 +0700

    Update TeleportSystem.lua

--- Features/TeleportSystem.lua
+++ Features/TeleportSystem.lua
@@ -41,8 +41,8 @@ print("[YOKUDO] Event found:", Event.ClassName)
 local TARGET_ID = nil
 local SAFE_ZONE = Vector3.new(533, 70, -366)
 
-local FLY_SPEED = 1100
-local RETURN_SPEED = 900
+local FLY_SPEED = 1000
+local RETURN_SPEED = 1000
 local FLY_OFFSET = 3
 local SHOT_DISTANCE = 30
 local ARRIVE_DISTANCE = 2
